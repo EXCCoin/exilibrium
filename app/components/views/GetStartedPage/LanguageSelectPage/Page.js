957ba37b5b3463acd7bfa17cf2364a358852f2d6
jwjanus <jacek.janus@pragmaticcoders.com>
2018-06-07 10:23:33 +0200

    Removed unnecessary logo from LanguageSelectPage

--- app/components/views/GetStartedPage/LanguageSelectPage/Page.js
+++ app/components/views/GetStartedPage/LanguageSelectPage/Page.js
@@ -11,7 +11,6 @@ const LanguageSelectPage = ({
 }) => {
   return (
     <div className="page-body getstarted">
-      <div className="getstarted-logo" />
       <div className="getstarted-new">
         <div className="language-select-title">
           <T id="selectLang.title" m={"Welcome to Exilibrium Wallet"} />
