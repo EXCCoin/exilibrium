db64a49b7cf279fc1a74ea521388d648d4513611
jwjanus <jacek.janus@pragmaticcoders.com>
2018-06-25 17:02:43 +0200

    Changed MainNetParams in wallet/constants

--- app/wallet/constants.js
+++ app/wallet/constants.js
@@ -10,10 +10,10 @@ export const TestNetParams = {
 };
 
 export const MainNetParams = {
-  TicketMaturity: 256,
-  TicketExpiry: 40960, // 5*TicketPoolSize
-  CoinbaseMaturity: 256,
+  TicketMaturity: 16,
+  TicketExpiry: 6144, // 5*TicketPoolSize
+  CoinbaseMaturity: 16,
   SStxChangeMaturity: 1,
   GenesisTimestamp: 1454954400,
-  TargetTimePerBlock: 5 * 60 // in seconds
+  TargetTimePerBlock: 2.5 * 60 // in seconds
 };
