cd681eecf512c261cc570dd1e3b3338a413ccf44
jwjanus <jacek.janus@pragmaticcoders.com>
2018-06-29 10:51:03 +0200

    Update "obtaining excc" link

--- app/components/views/HomePage/tables/NoTransactionsLinks.js
+++ app/components/views/HomePage/tables/NoTransactionsLinks.js
@@ -7,7 +7,7 @@ export default () => (
     <Link to="/transactions/receive" className="receive">
       <T id="home.noTransactions.receiveLink" m="Generate a EXCC Address for receiving funds" /> →
     </Link>
-    <ExternalLink href="https://docs.excc.co/getting-started/obtaining-excc/" className="buy">
+    <ExternalLink href="https://excc.co/#invest_section" className="buy">
       <T id="home.noTransactions.buyFromExchanges" m="Buy EXCC from Exchanges" /> →
     </ExternalLink>
   </div>
