cb689a8cc2c3a6549f09b5636bffffc186813f2a
jwjanus <jacek.janus@pragmaticcoders.com>
2018-07-10 14:01:00 +0200

    Update win32ipc test

--- app/modules/win32ipc/moduleTest.js
+++ app/modules/win32ipc/moduleTest.js
@@ -2,10 +2,14 @@ const childProcess = require("child_process");
 const util = require("util");
 const addon = require("./build/Release/win32ipc");
 
-var pipe = addon.createPipe("out");
+const pipe = addon.createPipe("out");
 console.log(pipe.readEnd, pipe.writeEnd);
 
-childProcess.spawn("dcrd", [ "--testnet", util.format("--piperx=%d", pipe.readEnd) ],
-  { "detached": true, "shell": true });
+childProcess.spawn("exccd", ["--testnet", util.format("--piperx=%d", pipe.readEnd)], {
+  detached: true,
+  shell: true
+});
 
-setTimeout(function () { process.exit(0); }, 10000);
+setTimeout(() => {
+  process.exit(0);
+}, 10000);
