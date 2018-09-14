df041b11916176d5fef58042a2a80fee0078f6bc
jwjanus <jacek.janus@pragmaticcoders.com>
2018-09-14 16:55:23 +0200

    Update social media links

--- app/components/views/HelpPage/LinksTab/index.js
+++ app/components/views/HelpPage/LinksTab/index.js
@@ -16,19 +16,22 @@ export const LinksTabHeader = () => (
 
 export const LinksTab = () => (
   <div className="help-icons-list">
+    <HelpLink className={"help-discord-icon"} href="https://discordapp.com/invite/pZe2EcH">
+      <T id="help.discord" m="Discord" />{" "}
+    </HelpLink>
     <HelpLink className={"help-common"} href="https://support.excc.co">
       <T id="help.support" m="Support" />{" "}
-    </HelpLink>
-    <HelpLink className={"help-github-icon"} href="https://github.com/EXCCoin/exilibrium">
-      <T id="help.github" m="Github" />
     </HelpLink>
     <HelpLink className={"help-twitter-icon"} href="https://twitter.com/EXCC_Team">
       <T id="help.twitter" m="Twitter" />{" "}
     </HelpLink>
     <HelpLink
       className={"help-forum-icon"}
-      href="https://bitcointalk.org/index.php?topic=2375067.msg24224448#msg24224448">
+      href="https://bitcointalk.org/index.php?topic=5023881.new">
       <T id="help.forum" m="Bitcointalk" />{" "}
+    </HelpLink>
+    <HelpLink className={"help-github-icon"} href="https://github.com/EXCCoin">
+      <T id="help.github" m="Github" />
     </HelpLink>
     <HelpLink className={"help-stakepools-icon"} href="https://stakepool.excc.co/">
       <T id="help.stakepools" m="Stakepool" />
