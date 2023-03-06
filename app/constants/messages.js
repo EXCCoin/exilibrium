28aefe15080c52d8e948f604d50c657311dbfe6e
widelec-BB <13343653-widelec-BB@users.noreply.gitlab.com>
2023-03-06 14:14:15 +0100

    Rebrand some messages

--- app/constants/messages.js
+++ app/constants/messages.js
@@ -5,7 +5,7 @@ export const SPV_CONNECT_WITHOUT_SPV =
   "Launch with the --spv option in order to use --spvconnect";
 
 export const RPC_WITHOUT_ADVANCED_MODE =
-  "Use --advanced to connect to a dcrd instance manually via PRC";
+  "Use --advanced to connect to a exccd instance manually via PRC";
 
 export const RPCCONNECT_INVALID_FORMAT =
   'Please provide a valid host and port for --rpcconnect, eg. --rpcconnect="127.0.0.1:19109"';
@@ -17,7 +17,7 @@ export const SPV_WITH_ADVANCED_MODE =
   "Cannot use SPV mode and advanced daemon mode at the same time";
 
 export const DIFF_CONNECTION_ERROR =
-  "daemon and decrediton does not have the same network";
+  "daemon and Exilibrium does not have the same network";
 
 // POSITION_ERROR and MISMATCH_ERROR represent errors when creating or
 // restoring wallet with seed.
